ffa31b41c124ef3e25b100b410670591e13d0341
Jh0nn1er <jhonnier.gomez@finanzauto.com.co>
2026-04-17 23:18:13 -0500

    Fix(Vite): modify config vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,17 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
+  server: {
+    host: true,        // 🔥 permite acceso desde Windows (WSL fix)
+    port: 5173,        // puedes cambiarlo si quieres
+    strictPort: true,  // evita que cambie el puerto automáticamente
+    open: false,       // evita abrir navegador en WSL
+  }
 })
